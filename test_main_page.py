247fc0699877678cca493e7241906a22e7dbfa7c
OksanaIzi <73961325+OksanaIzi@users.noreply.github.com>
2020-11-27 14:35:11 +0300

    убран лишний закомментрованный код

--- test_main_page.py
+++ test_main_page.py
@@ -8,14 +8,13 @@ import pytest
 class TestLoginFromMainPage():
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
-        page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-        page.open()                      # открываем страницу
-        page.should_be_login_link()      # выполняем метод страницы — переходим на страницу логина
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
         page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
-        #login_page = page.go_to_login_page()   Возвразается обьект LoginPage
-        #login_page.should_be_login_page()    применяется метод тест проверки к новой странице
+
 
     def test_guest_should_see_login_link(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
